20b48f01208e47ff56203f539c3dc2b85c927b21
albracu <yo@alfredobravocuero.co>
2017-12-01 15:49:33 -0500

    Introduccion y devolucion de datos en pantalla

---
+++ src/entrada/Entrada.java
@@ -0,0 +1,24 @@
+package entrada;
+
+import java.util.Scanner;
+
+public class Entrada {
+    public static void main(String[] args) {
+        // Es este segmento introducimos datos
+        Scanner primeraentrada = new Scanner(System.in);
+
+        // Devolvemos los datos en pantalla
+        System.out.println(primeraentrada.nextLine());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
